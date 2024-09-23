30f11ab0d1e39c46b89a47476cf16dc8aea0955b
Brian Holt <bholt@dwolla.com>
2024-09-23 15:27:21 -0500

    modify constructor to only use binary-stable types

--- src/main/scala/com/dwolla/config/package.scala
+++ src/main/scala/com/dwolla/config/package.scala
@@ -1,27 +1,31 @@
 package com.dwolla
 
-import cats.*
+import cats.effect.*
 import cats.syntax.all.*
 import com.amazonaws.kms.{CiphertextType, KMS}
+import fs2.compression.Compression
 import monix.newtypes.NewtypeWrapped
 import mouse.all.*
 import pureconfig.ConfigReader
 import smithy4s.Blob
+import smithy4s.aws.{AwsClient, AwsEnvironment}
 
 package object config {
   private[this] val secureStringRegex = "^SECURE: (.+)".r
 
-  def SecureReader[F[_] : MonadThrow](kms: KMS[F]): ConfigReader[F[SecurableString]] =
-    ConfigReader[String].map {
-      case secureStringRegex(cryptotext) =>
-        kms.decrypt(CiphertextType(Blob(cryptotext.getBytes())))
-          .map(_.plaintext) // TODO does this need to be base64-decoded?
-          .liftOptionT
-          .getOrRaise(new RuntimeException("boom")) // TODO convert to a better exception
-          .map(_.value.toUTF8String)
-          .map(SecurableString(_))
+  def SecureReader[F[_] : Async : Compression](awsEnv: AwsEnvironment[F]): Resource[F, ConfigReader[F[SecurableString]]] =
+    AwsClient(KMS, awsEnv).map { kms =>
+      ConfigReader[String].map {
+        case secureStringRegex(cryptotext) =>
+          kms.decrypt(CiphertextType(Blob(cryptotext.getBytes())))
+            .map(_.plaintext) // TODO does this need to be base64-decoded?
+            .liftOptionT
+            .getOrRaise(new RuntimeException("boom")) // TODO convert to a better exception
+            .map(_.value.toUTF8String)
+            .map(SecurableString(_))
 
-      case s => SecurableString(s).pure[F]
+        case s => SecurableString(s).pure[F]
+      }
     }
 
   type SecurableString = SecurableString.Type
